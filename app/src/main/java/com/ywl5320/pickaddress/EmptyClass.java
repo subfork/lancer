58ca4b9deee5efe2981487cbea7b5deddb84caa8
subfork <boyluoxing@163.com>
2016-09-14 11:56:56 +0800

    some changes for files not to commit

--- app/src/main/java/com/ywl5320/pickaddress/EmptyClass.java
+++ app/src/main/java/com/ywl5320/pickaddress/EmptyClass.java
@@ -7,4 +7,5 @@ package com.ywl5320.pickaddress;
 public class EmptyClass {
 	//this is a change to commit
 	/*this is added in android studio but submit on github desktop sha=2b4858f1e4cadf971be53fdb0cb7b796a8890975*/
+    /*a new line to coammit*/
 }
